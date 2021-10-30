77592ab8f8d28c2960b065f7c8b90fc191c9c1cc
Joseph <mate.joseph@gmail.com>
2021-10-29 22:55:06 -0400

    solving the queries in order of steps was enough to pass test3

--- 1602/b/Main.java
+++ 1602/b/Main.java
@@ -2,37 +2,41 @@ import java.util.*;
 
 public class Main {
   
-  private static void query(
-          int [] initialValues,
+  private static int query(
+          StepTracker stepTracker,
           int a_x,
           int steps
   ) {
-    int [] current = Arrays.copyOf(initialValues, initialValues.length);
-    for (int i = 0; i < steps; i++) {
+    if (stepTracker.repeated) {
+      return stepTracker.current[a_x];
+    }
+
+    for (int i = stepTracker.step; i < steps; i++) {
       Map<Integer, Integer> counts = new HashMap<>();
-      for (int j = 0; j < current.length; j++) {
-        counts.put(current[j], counts.getOrDefault(current[j], 0) + 1);
+      for (int j = 0; j < stepTracker.current.length; j++) {
+        counts.put(stepTracker.current[j], counts.getOrDefault(stepTracker.current[j], 0) + 1);
       }
 
-      int [] prev = current;
-      current = new int[initialValues.length];
-      for (int j = 0; j < current.length; j++) {
-        current[j] = counts.getOrDefault(prev[j], 0);
+      int [] prev = stepTracker.current;
+      stepTracker.current = new int[stepTracker.current.length];
+      for (int j = 0; j < stepTracker.current.length; j++) {
+        stepTracker.current[j] = counts.getOrDefault(prev[j], 0);
       }
+      stepTracker.step++;
 
       // check if prev is the same as current. if so then the result will never change
       boolean same = true;
-      for (int j = 0; j < initialValues.length; j++) {
-        if (prev[j] != current[j]) {
+      for (int j = 0; j < stepTracker.current.length; j++) {
+        if (prev[j] != stepTracker.current[j]) {
           same = false;
         }
       }
       if (same) {
-        System.out.println(current[a_x]);
-        return;
+        stepTracker.repeated = true;
+        return stepTracker.current[a_x];
       }
     }
-    System.out.println(current[a_x]);
+    return stepTracker.current[a_x];
   }
 
   public static void main(String [] args) { 
@@ -45,12 +49,50 @@ public class Main {
           values[j] = scanner.nextInt();
         }
         int numQueries = scanner.nextInt();
+
+        // process the queries in the order of steps
+        List<Query> queries = new ArrayList<>(numQueries);
         for (int j = 0; j < numQueries; j++) {
-          int a_x = scanner.nextInt();
-          int step = scanner.nextInt();
-          query(values, a_x-1, step);
+          Query query = new Query();
+          query.posn = j;
+          query.a_x = scanner.nextInt();
+          query.steps = scanner.nextInt();
+          queries.add(query);
+        }
+        Collections.sort(queries, Comparator.comparing(Query::getSteps));
+        StepTracker stepTracker = new StepTracker();
+        stepTracker.current = values;
+        for (Query query : queries) {
+          query.result = query(stepTracker, query.a_x-1, query.steps);
+        }
+
+        // output in the order of input
+        Collections.sort(queries, Comparator.comparing(Query::getPosn));
+        for (Query query : queries) {
+          System.out.println(query.result);
         }
       }
+    }
+  }
+
+  private static class StepTracker {
+    int step = 0;
+    boolean repeated = false;
+    int [] current;
+  }
+
+  private static class Query {
+    int posn;
+    int a_x;
+    int steps;
+    int result;
+
+    public int getSteps() {
+      return steps;
+    }
+
+    public int getPosn() {
+      return posn;
     }
   }
 }
